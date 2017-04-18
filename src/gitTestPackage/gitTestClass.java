e7c8817acd3b1f94f3584c0bc882e013ec643c87
PC219-21\NTUT <weteresa@gmail.com>
2017-04-18 10:57:15 +0800

    no message

--- src/gitTestPackage/gitTestClass.java
+++ src/gitTestPackage/gitTestClass.java
@@ -5,7 +5,7 @@ public class gitTestClass {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!");
 
-		System.out.println("HelloWorld!!");
+		System.out.println("HelloMaster!!");
 
 	}
 
